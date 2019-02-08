c783177724a05076b12fc47f430aae88b5b1f7e2
@ChikaraK <nowano0403@gmail.com>
2019-02-08 16:27:08 +0900

    [finish]4-8_Bookers

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -1,13 +1,52 @@
 class BookersController < ApplicationController
-  def show
+  def top
   end
 
   def index
+  	#一覧表示用
+  	@bookers = Booker.all
+  	# form-for用
+  	@booker = Booker.new
   end
-
-  def new
+  def create
+  	@booker = Booker.new(booker_params)
+  	if @booker.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to booker_path(@booker)
+    else
+      render action: :top
+    end
+  end
+  def show
+  	@booker = Booker.find(params[:id])
   end
 
   def edit
+  	@booker = Booker.find(params[:id])
+  end
+  def update
+  	@booker = Booker.find(params[:id])
+    if @booker.update(booker_params)
+      flash[:notice] = "Book was succesfully updated"
+  	  redirect_to booker_path(@booker)
+    else
+      render action:top
+    end
+  end
+  def destroy
+    @booker = Booker.find(params[:id])
+  	if @booker.destroy
+      flash[:notice] = "Book was succesfully deleted"
+  	  redirect_to bookers_path
+    else
+      render action:top
+    end
+  end
+
+
+  private
+
+  def booker_params
+  	params.require(:booker).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookers/show'
-  get 'bookers/index'
-  get 'bookers/new'
-  get 'bookers/edit'
+  root 'bookers#top'
+  resources :bookers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
